5fb5682c199f1e868c44369fb4cc290870057f6f
Edwin <chenwenzhe88@gmail.com>
2023-03-08 19:57:52 -0800

    Add react (#4)
    Co-authored-by: Edwin Chen <chenwenzhe88@EdwinsMacStudio.socal.rr.com>

---
+++ my-app/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: { noEmit: false },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    publicPath: '',
+    filename: "content2.js",
+    path: path.resolve(__dirname, "..", "scripts"),
+  },
+};
