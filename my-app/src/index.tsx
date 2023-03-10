bbc48273943580db2d14358757a1516b5c652a03
Youming Li <youmin.arthur@gmail.com>
2023-03-09 23:52:52 -0800

    clean

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,5 +1,5 @@
 /*global chrome*/
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
